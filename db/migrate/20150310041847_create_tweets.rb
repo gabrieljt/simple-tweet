63670fbe28fd11ed220e7d4081d1b3b40e1df09d
Gabriel Jordão Trabasso <gabrieljt@gmail.com>
2015-03-10 01:23:21 -0300

    rails g model Tweet

---
+++ db/migrate/20150310041847_create_tweets.rb
@@ -0,0 +1,11 @@
+class CreateTweets < ActiveRecord::Migration
+  def change
+    create_table :tweets do |t|
+      t.references :user, required: true, index: true
+      t.string :content, null: false
+
+      t.timestamps null: false
+    end
+    add_foreign_key :tweets, :users
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150310030653) do
+ActiveRecord::Schema.define(version: 20150310041847) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "content",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "tweets", ["user_id"], name: "index_tweets_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "username",   null: false
