1e1c8c5b23d25f9bef08e36ecf277f99e5ca51f4
Gabriel Jordão Trabasso <gabrieljt@gmail.com>
2015-03-10 15:18:21 -0300

    rails g controller sessions and routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'users#new'
 
+  get :login, to: 'sessions#new', as: :login
+  post :login, to: 'sessions#create', as: :new_login
+  delete :logout, to: 'sessions#destroy', as: :logout
+
   resources :users, only: [:new, :create, :show]
   resources :tweets, only: [:index, :create]
 end
