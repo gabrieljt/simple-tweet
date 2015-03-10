7868c94db0d99116c0a5217586d754e290e5f7fb
Gabriel Jordão Trabasso <gabrieljt@gmail.com>
2015-03-10 15:41:47 -0300

    simple session login and specs; TODO: verify hashed password.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,20 @@
 class SessionsController < ApplicationController
   def new
+    @user = User.new
+    respond_with @user
   end
 
   def create
+    @user = User.new session_params
+    User.exists? username: @user.username ? redirect_to(tweets_path) : render(:new)
   end
 
   def destroy
+  end
+
+  private
+
+  def session_params
+    params.require(:user).permit(:username, :password)
   end
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,11 +2,46 @@ require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
 
-  describe "GET #new" do
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
+  let(:valid_attributes) { create(:user).attributes }
+  let(:invalid_attributes) { attributes_for :invalid_user }
+  let(:valid_session) { {} }
+
+  describe "GET new" do
+    it "assigns a new user as @user" do
+      get :new, {}, valid_session
+
+      expect(assigns :user ).to be_a_new User
     end
   end
 
+  describe "POST create" do
+    context "with valid params" do
+
+      it "assigns a logged in user as @user" do
+        post :create, { user: valid_attributes }, valid_session
+
+        expect(assigns :user).to be_a User
+      end
+
+      it "redirects to the tweets timeline" do
+        post :create, { user: valid_attributes }, valid_session
+
+        expect(response).to redirect_to tweets_path
+      end
+    end
+
+    context "with invalid params" do
+      it "assigns a newly created but unsaved user as @user" do
+        post :create, { user: invalid_attributes }, valid_session
+
+        expect(assigns :user).to be_a_new User
+      end
+
+      it "re-renders the 'new' template" do
+        post :create, { user: invalid_attributes }, valid_session
+
+        expect(response).to render_template :new
+      end
+    end
+  end
 end
