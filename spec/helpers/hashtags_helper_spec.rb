2aa715f9a239a77c914a40b1d525cf323ebe9987
Gabriel Jordão Trabasso <gabrieljt@gmail.com>
2015-03-11 00:26:47 -0300

    HashtagsHelper fully covered

--- spec/helpers/hashtags_helper_spec.rb
+++ spec/helpers/hashtags_helper_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe HashtagsHelper, :type => :helper do
   before do @tweet = create :tweet, message: 'oh, hi! #justTesting works? #yeah!' end
 
-  describe '#parse_input' do
+  describe '#parse_input_for' do
     it 'finds and creates Hashtags' do
       expect { parse_input_for tweet: @tweet }.to change(Hashtag, :count).by 2
     end
@@ -19,6 +19,11 @@ RSpec.describe HashtagsHelper, :type => :helper do
     end
   end
 
-  describe '#parse_output' do
+  describe '#parse_output_for' do
+    it 'finds and replaces keywords with link_to keyword, hashtag_path' do
+      parse_input_for tweet: @tweet
+      parse_output_for tweet: @tweet
+      expect(@tweet.message).to include '<a href="/hashtags/1">#justTesting</a>', '<a href="/hashtags/2">#yeah!</a>'
+    end
   end
 end
